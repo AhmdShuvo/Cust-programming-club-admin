a73ffe30ee5b1412bbe53574b66cfb76f7b150dd
Tanjir Ahmed <74643721+AhmdShuvo@users.noreply.github.com>
2022-09-12 22:26:36 +0600

    delete notice

--- src/Components/Dashboard/PostNotice/DeleteNotice.js
+++ src/Components/Dashboard/PostNotice/DeleteNotice.js
@@ -3,7 +3,12 @@ import React from 'react';
 const DeleteNotice = () => {
     return (
         <div>
-            <h1>Delete Notice</h1>
+            <h1>Delete Notice
+
+
+
+                <h3>skdjfsldifsodgslkcvjskvjsvs</h3>
+            </h1>
         </div>
     );
 };
