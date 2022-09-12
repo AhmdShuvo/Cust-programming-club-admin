14996ae36c5d9abcb8911488995abb145d065cbc
Tanjir Ahmed <74643721+AhmdShuvo@users.noreply.github.com>
2022-09-12 22:24:06 +0600

    delete notice

---
+++ src/Components/Dashboard/PostNotice/DeleteNotice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeleteNotice = () => {
+    return (
+        <div>
+            <h1>Delete Notice</h1>
+        </div>
+    );
+};
+
+export default DeleteNotice;
